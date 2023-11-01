1fab11dc72166b7db26716cc13ae8f1b7e8d4445
iamsoojung <crystar2402@naver.com>
2023-11-02 03:26:53 +0900

    front, back 간 연동 테스트

---
+++ src/main/java/com/example/mltserver/MltServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mltserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MltServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MltServerApplication.class, args);
+	}
+
+}
